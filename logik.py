9ec55fbc2a29672ad2d6cbecd7741bb5e89516e1
Noé Leroi <nleroi.pro@gmail.com>
2022-10-06 16:34:18 +0200

    first commit

---
+++ logik.py
@@ -0,0 +1,11 @@
+name = "John Cena"
+birthdate = "30-05-2001"
+letters = ["Ford", "Volvo", "BMW"] 
+
+def sortName(ToRemoveSpaces): 
+    ToRemoveSpaces.replace(" ", "")
+
+
+sortName(name)
+
+print(name)
